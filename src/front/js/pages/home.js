40753c1cbc249b73d33fe55b0b945737467a4439
Manuel Labarca <labarca.manu@gmail.com>
2021-09-11 09:39:27 +0000

    map

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from "@react-google-maps/api";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 });
+	const [bounds, setBounds] = useState(null);
+	const [markers, setMarkers] = useState([]);
+	const [map, setMap] = useState(null);
 
-	return (
+	const { isLoaded } = useJsApiLoader({
+		id: "google-map",
+		googleMapsApiKey: "A"
+	});
+
+	const onLoad = map => {
+		navigator.geolocation.getCurrentPosition(({ coords: { latitude: lat, longitude: lng } }) => {
+			const pos = { lat, lng };
+			setCurrentLocation(pos);
+		});
+		const bounds = new window.google.maps.LatLngBounds();
+		map.fitBounds(bounds);
+		setMap(map);
+	};
+
+	const SBonLoad = ref => (this.searchBox = ref);
+
+	const onPlacesChanged = () => console.log(this.searchBox.getPlaces());
+
+	return isLoaded ? (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<GoogleMap
+				onLoad={map => onLoad(map)}
+				zoom={10}
+				center={currentLocation}
+				mapContainerStyle={{ height: "400px", width: "800px" }}>
+				<InfoWindow position={currentLocation}>
+					<div>
+						<h1>Infoooo</h1>
+					</div>
+				</InfoWindow>
+				<Marker label="Mi posición" position={currentLocation} />
+			</GoogleMap>
 		</div>
+	) : (
+		<></>
 	);
 };
